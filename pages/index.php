8e9bc040a260da06d7e00ff0e7395ae3868224d3
Joshua <joshuahelfferich@hotmail.com>
2017-10-02 14:04:33 +0200

    Revert "Test to see if site auto update works"
    This reverts commit e01f9971f0049e449eefdf99f4beef269bcfd48e.

--- pages/index.php
+++ pages/index.php
@@ -24,14 +24,4 @@
 			</div>
 		</div>
 	</div>
-	
-	<div class="col-md-6">
-		<div class="card">
-			<div class="card-body">
-				<h3 class="card-title">Title for the test thing</h3>
-				<p class="card-text">This is the thing. This is filler text. There is no content as of now. We are working on it. Please be patient. This is the thing. This is filler text. There is no content as of now. We are working on it. Please be patient. This is the thing. This is filler text. There is no content as of now. We are working on it. Please be patient. </p>
-				<a class="btn btn-primary" href="#" role="button">View details &raquo;</a>
-			</div>
-		</div>
-	</div>
 </div>
